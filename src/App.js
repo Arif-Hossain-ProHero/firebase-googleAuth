3e03750634f7de307fae5783cd32c4bfe95b8d95
Arif-Hossain-ProHero <arif.programminghero@gmail.com>
2021-10-07 01:10:00 +0600

    authentication added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import "./App.css";
+import initializeAuth from "./Firebase/firebase.init";
+import { useState } from "react";
+
+initializeAuth();
+const provider = new GoogleAuthProvider();
 
 function App() {
+  const [user, setUser] = useState({});
+
+  const handleGoogleSignIn = () => {
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const { displayName, email, photoURL } = result.user;
+        const loggedUser = {
+          name: displayName,
+          email: email,
+          photo: photoURL,
+        };
+        setUser(loggedUser);
+      })
+      .catch((error) => console.log(error.message));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleGoogleSignIn}>SignIn</button>
+      <br />
+      {user.email && (
+        <div>
+          <h1>{user.name}</h1>
+          <h5>{user.email}</h5>
+          <img src={user.photo} alt="" />
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBK_JF0RcR2Fn1qUuupOrZVDmEev-K_5jk",
+  authDomain: "first-firebase-project-d332a.firebaseapp.com",
+  projectId: "first-firebase-project-d332a",
+  storageBucket: "first-firebase-project-d332a.appspot.com",
+  messagingSenderId: "1047883902119",
+  appId: "1:1047883902119:web:b56b9f9d4dd15a5438bf82",
+};
+
+export default firebaseConfig;
